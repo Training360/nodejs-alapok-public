271b550e2b0c7be0c444f337e8629e1bdeedeaea
Pryann <pryann@gmail.com>
2021-04-12 15:59:50 +0200

    Global and module objects

---
+++ module-system/global-and-modules-objects/app.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-var
+var userName = 'John Doe'
+console.log('username', userName)
+console.log('global username', global.userName)
+console.log(global)
+console.log(module)
