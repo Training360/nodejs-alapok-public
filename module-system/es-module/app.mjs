3dff6323b762254f854cfa26ebf524be3a49c5d8
Pryann <pryann@gmail.com>
2021-04-13 13:14:59 +0200

    use native ES module system

---
+++ module-system/es-module/app.mjs
@@ -0,0 +1,7 @@
+import * as utils from './utils.mjs'
+// import { even, odd } from './utils.mjs'
+
+console.log(utils)
+const priceList = [345, 67, 234, 57, 236, 456]
+console.log(utils.even(priceList))
+console.log(utils.odd(priceList))
---
+++ module-system/es-module/utils.mjs
@@ -0,0 +1,2 @@
+export const even = arr => arr.filter(item => item % 2 === 0)
+export const odd = arr => arr.filter(item => item % 2 !== 0)
