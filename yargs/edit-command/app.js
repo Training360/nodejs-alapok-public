18132a80af8ab72be0b814b404935469cd515603
Pryann <pryann@gmail.com>
2021-04-18 19:52:05 +0200

    yargs edit command

---
+++ yargs/edit-command/app.js
@@ -0,0 +1,82 @@
+const yargs = require('yargs')
+let movies = require('./database/movies')
+
+yargs
+  .version('1.0.0')
+  .usage('Usage: <command> [options]')
+  .command({
+    command: 'get',
+    describe: 'Get all movies',
+    handler: () => console.log(movies)
+  })
+  .command({
+    command: 'find',
+    describe: 'Find a movie by id',
+    builder: {
+      id: {
+        alias: 'i',
+        describe: 'Movie ID',
+        type: 'number',
+        demandOption: true
+      }
+    },
+    handler: ({ id }) =>
+      console.log(movies.find(movie => movie.id === id))
+  })
+  .command({
+    command: 'create',
+    describe: 'Create a new movie',
+    builder: {
+      producer: {
+        alias: 'p',
+        describe: 'Film producer',
+        type: 'string',
+        demandOption: true
+      },
+      title: {
+        alias: 't',
+        describe: 'Movie title',
+        type: 'string',
+        demandOption: true
+      }
+    },
+    handler: ({ title, producer }) => {
+      const sortedMovies = [...movies].sort((a, b) => a.id > b.id)
+      const id = sortedMovies[sortedMovies.length - 1].id + 1
+      const movie = { id, title, producer }
+      movies = [...movies, movie]
+      console.log(movies.find(movie => movie.id === id))
+    }
+  })
+  .command({
+    command: 'edit',
+    describe: 'Edit a movie',
+    builder: {
+      id: {
+        alias: 'i',
+        describe: 'Movie ID',
+        type: 'number',
+        demandOption: true
+      },
+      producer: {
+        alias: 'p',
+        describe: 'Film producer',
+        type: 'string',
+        demandOption: true
+      },
+      title: {
+        alias: 't',
+        describe: 'Movie title',
+        type: 'string',
+        demandOption: true
+      }
+    },
+    handler: ({ id, producer, title }) => {
+      movies = movies.map(movie => movie.id === id ? { id, producer, title } : movie)
+      console.log(movies.find(movie => movie.id === id))
+    }
+  })
+  .locale('en')
+  .strict()
+  .help()
+  .parse() // process.argv
