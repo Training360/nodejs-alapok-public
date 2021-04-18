44dc93df48f8b8cfbac252861df8287491e3d889
Pryann <pryann@gmail.com>
2021-04-18 18:11:56 +0200

    os module

---
+++ os/app.js
@@ -0,0 +1,11 @@
+const os = require('os')
+
+console.log('Platform: ', os.platform())
+console.log('Arch.: ', os.arch())
+console.log('OS version: ', os.version())
+console.log('OS build number: ', os.release())
+console.log('Processor: ', os.cpus()[0].model)
+console.log('Total memory size: ', (os.totalmem() / 1024 / 1024 / 1024).toFixed(2), 'GB')
+console.log('Free memory size: ', (os.freemem() / 1024 / 1024 / 1024).toFixed(2), 'GB')
+console.log('Username', os.userInfo().username)
+console.log('IP address', os.networkInterfaces().Ethernet[1].address)
