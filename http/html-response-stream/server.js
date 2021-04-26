5f858d0aa73bf9492f741537d0175413e46a338d
Pryann <pryann@gmail.com>
2021-04-26 15:16:26 +0200

    html response and response stream

---
+++ http/html-response-stream/server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const { readFileSync } = require('fs')
+const port = 8080
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+  const html = readFileSync('./index.html', 'utf-8')
+  res.end(html)
+}).listen(port)
+
+console.log(`Server running at http://127.0.0.1:${port}`)
