479cdfb0723b28718bf10e01d557a464ce9a6518
Pryann <pryann@gmail.com>
2021-04-26 17:53:40 +0200

    http error page

---
+++ http/error-page/server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const { createReadStream } = require('fs')
+
+const port = 8080
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    createReadStream('./views/index.html').pipe(res)
+  } else if (req.url === '/about') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    createReadStream('./views/about.html').pipe(res)
+  } else if (req.url === '/contact') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    createReadStream('./views/contact.html').pipe(res)
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html'
+    })
+    createReadStream('./views/404.html').pipe(res)
+  }
+}).listen(port)
+
+console.log(`Server is running at http://127.0.0.1:${port}`)
+
+// http://127.0.0.1 - index.html
+// http://127.0.0.1/about - about.html
+// http://127.0.0.1/contact - contact.html
