55c2aa838ef8e48ac8d5fb304ce09d7fe85b423b
Pryann <pryann@gmail.com>
2021-04-26 15:21:07 +0200

    fix html response files

--- http/html-response-stream/server.js
+++ http/html-response-stream/server.js
@@ -1,13 +1,12 @@
 const http = require('http')
-const { readFileSync } = require('fs')
+const { createReadStream } = require('fs')
 const port = 8080
 
 http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   })
-  const html = readFileSync('./index.html', 'utf-8')
-  res.end(html)
+  createReadStream('./index.html').pipe(res)
 }).listen(port)
 
 console.log(`Server running at http://127.0.0.1:${port}`)
